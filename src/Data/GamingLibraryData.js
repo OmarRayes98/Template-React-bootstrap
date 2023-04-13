7cfd2a6165da39306f8ead16891af63608c0d2c0
OmarRayes98 <omarrayes1998@gmail.com>
2023-04-13 22:27:45 +0300

    second-commit

--- src/Data/GamingLibraryData.js
+++ src/Data/GamingLibraryData.js
@@ -1,7 +1,6 @@
-import popular_01 from '../assets/images/game-01.jpg';
-import popular_02 from '../assets/images/game-02.jpg';
-import popular_03 from '../assets/images/game-03.jpg';
-
+import popular_01 from '../assets/images/popular-01.jpg';
+import popular_02 from '../assets/images/popular-02.jpg';
+import popular_03 from '../assets/images/popular-03.jpg';
 
 const GamingLibraryData = [
     {id:1 , image: popular_01 ,title:"Dota 2" ,category: "Sandbox" , date_added :"24/08/2025",hours_played:"634 H 22 Mins"   ,download:"2.3M" },
--- src/components/GamingLibraryCards.jsx
+++ src/components/GamingLibraryCards.jsx
@@ -6,7 +6,7 @@ const GamingLibraryCards = (props) => {
 return (
     <div className='gaming-library-card'>
         <ul>
-            <li><img src={props.image}/></li>
+            <li className='gaming-library-card-image' ><img src={props.image}/></li>
             <li><h4>{props.title}</h4><span>{props.category}</span></li>
             <li><h4>Date Added</h4><span>{props.date_added}</span></li>
             <li><h4>Hours Played</h4><span>{props.hours_played}</span></li>
